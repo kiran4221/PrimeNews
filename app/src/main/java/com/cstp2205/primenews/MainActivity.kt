97d5d2dc14da98154269f7cf8602ec1cf250aa80
Kiranpreet Kaur <000475341@student.vcc.ca>
2025-03-18 13:55:13 -0700

    Resolved issue for the deleted files( added them back)

--- app/src/main/java/com/cstp2205/primenews/MainActivity.kt
+++ app/src/main/java/com/cstp2205/primenews/MainActivity.kt
@@ -15,11 +15,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
 import com.cstp2205.primenews.ui.screens.auth.SignInScreen
 import com.cstp2205.primenews.ui.screens.auth.SignUpScreen
 import com.cstp2205.primenews.ui.screens.news.NewsScreen
 import com.cstp2205.primenews.ui.theme.PrimeNewsTheme
 import com.google.firebase.auth.ActionCodeUrl
+import com.cstp2205.primenews.viewmodel.NewsViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.cstp2205.primenews.data.model.Article
+import com.cstp2205.primenews.ui.screens.news.ArticleDetailScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +32,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrimeNewsTheme {
-                    PrimeNewsApp()
+                PrimeNewsApp()
             }
         }
     }
@@ -35,26 +40,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PrimeNewsApp() {
-
-   var currentScreen by remember { mutableStateOf("signIn")}
+    var currentScreen by remember { mutableStateOf("signIn") }
+    val newsViewModel: NewsViewModel = viewModel()
+    var selectedArticle by remember { mutableStateOf<Article?>(null) }
 
     when (currentScreen) {
         "signIn" -> SignInScreen(
-            onSignInSuccess = {currentScreen = "newsScreen"},
-            onNavigateToSignUp = {currentScreen = "signUp"}
+            onSignInSuccess = { currentScreen = "newsScreen" },
+            onNavigateToSignUp = { currentScreen = "signUp" }
         )
-
         "signUp" -> SignUpScreen(
-            onSignUpSuccess = {currentScreen = "newsScreen"},
-            onNavigateToSignIn = {currentScreen = "signIn"}
+            onSignUpSuccess = { currentScreen = "newsScreen" },
+            onNavigateToSignIn = { currentScreen = "signIn" }
         )
-
-        /*"newsScreen" -> NewsScreen(
-            onSignOut = { currentScreen = "signIn"}
-        )*/
+        "newsScreen" -> {
+            LaunchedEffect(Unit) {
+                newsViewModel.loadNews("8bb8e5a543b4418a807e4e69b3c4af4a")
+            }
+            NewsScreen(
+                articles = newsViewModel.articles,
+                onArticleClick = { article ->
+                    selectedArticle = article
+                    currentScreen = "detailScreen"
+                },
+                onSignOut = { currentScreen = "signIn" }
+            )
+        }
+        "detailScreen" -> {
+            selectedArticle?.let { article ->
+                ArticleDetailScreen(
+                    article = article,
+                    onSaveFavourite = { currentScreen = "newsScreen" },
+                    onBack = { currentScreen = "newsScreen" }
+                )
+            }
+        }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/cstp2205/primenews/ui/screens/news/ArticleDetailsScreen.kt
+++ app/src/main/java/com/cstp2205/primenews/ui/screens/news/ArticleDetailsScreen.kt
@@ -1,12 +1,15 @@
 package com.cstp2205.primenews.ui.screens.news
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,19 +47,27 @@ fun ArticleDetailScreen(
             ) {
                 Column {
                     Text(text = article.title ?: "No Title", style = MaterialTheme.typography.titleLarge)
-                    Spacer(modifier = Modifier.height(8.dp))
+                    Spacer(modifier = Modifier.height(10.dp))
                     if (article.urlToImage != null) {
                         Image(
                             painter = rememberAsyncImagePainter(article.urlToImage),
                             contentDescription = "Article Image",
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .height(200.dp),
+                                .height(200.dp)
+                                .aspectRatio(16f/9f)
+                                .clip(RoundedCornerShape(12.dp)),
                             contentScale = ContentScale.Crop
                         )
-                        Spacer(modifier = Modifier.height(8.dp))
+                        Spacer(modifier = Modifier.height(10.dp))
                     }
                     Text(text = article.content ?: "No Content Available", style = MaterialTheme.typography.bodyLarge)
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    Text(text = article.urlToImage ?: "No url available", style = MaterialTheme.typography.bodySmall)
+
+                    Spacer(modifier = Modifier.height(10.dp))
                 }
                 Button(
                     onClick = { onSaveFavourite(article) },
