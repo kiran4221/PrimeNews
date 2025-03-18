dbbbbd309a88c7bf9a69c95b03f8c2ca0d6be1b3
Kiranpreet Kaur <000475341@student.vcc.ca>
2025-03-18 14:45:01 -0700

    Improved the UI for Detailed Article page, added all the relevan stuff about the article

--- app/src/main/java/com/cstp2205/primenews/ui/screens/news/ArticleDetailsScreen.kt
+++ app/src/main/java/com/cstp2205/primenews/ui/screens/news/ArticleDetailsScreen.kt
@@ -10,7 +10,9 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
@@ -26,7 +28,7 @@ fun ArticleDetailScreen(
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Article Detail", fontSize = 20.sp) },
+                title = { Text("Article Details", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color(0xFF01579B)) },
                 navigationIcon = {
                     IconButton(onClick = onBack) {
                         Icon(
@@ -46,8 +48,8 @@ fun ArticleDetailScreen(
                 verticalArrangement = Arrangement.SpaceBetween
             ) {
                 Column {
-                    Text(text = article.title ?: "No Title", style = MaterialTheme.typography.titleLarge)
-                    Spacer(modifier = Modifier.height(10.dp))
+                    Text(text = article.title ?: "No Title", fontWeight = FontWeight.Bold, fontSize = 25.sp, style = MaterialTheme.typography.titleLarge,)
+                    Spacer(modifier = Modifier.height(15.dp))
                     if (article.urlToImage != null) {
                         Image(
                             painter = rememberAsyncImagePainter(article.urlToImage),
@@ -59,15 +61,37 @@ fun ArticleDetailScreen(
                                 .clip(RoundedCornerShape(12.dp)),
                             contentScale = ContentScale.Crop
                         )
-                        Spacer(modifier = Modifier.height(10.dp))
+                        Spacer(modifier = Modifier.height(15.dp))
                     }
-                    Text(text = article.content ?: "No Content Available", style = MaterialTheme.typography.bodyLarge)
 
                     Spacer(modifier = Modifier.height(10.dp))
 
-                    Text(text = article.urlToImage ?: "No url available", style = MaterialTheme.typography.bodySmall)
+                    Text(text = article.urlToImage ?: "No url available", style = MaterialTheme.typography.bodySmall, color = Color.Blue)
+
+                    Spacer(modifier = Modifier.height(15.dp))
+
+                    Text(text = article.author ?: "Author Name not given", style = MaterialTheme.typography.displaySmall)
 
                     Spacer(modifier = Modifier.height(10.dp))
+
+                    Text(text = article.publishedAt ?: "No info", style = MaterialTheme.typography.bodyMedium)
+
+                    Spacer(modifier = Modifier.height(12.dp))
+
+                    /*Text(text = article.content ?: "No Content Available", style = MaterialTheme.typography.titleSmall)
+
+                    Spacer(modifier = Modifier.height(10.dp))*/
+
+                    Text(text = article.description ?: "Description is not available", style = MaterialTheme.typography.bodyMedium)
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    Text(text = "To read full article, click the url below...")
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    Text(text = article.url ?: "No url available", style = MaterialTheme.typography.bodySmall, color = Color.Blue)
+
                 }
                 Button(
                     onClick = { onSaveFavourite(article) },
